4d69cbf73703854be4c95c360222641cdfea7a76
miaelii <lizmiao@ucdavis.edu>
2017-02-20 13:09:38 -0800

    Add files via upload

---
+++ studio5/DOMContentLoaded template_files/script.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+
+    console.log('reading');
+
+//capture the submit event
+  document.workout.onsubmit = processForm;
+
+  //define process function
+  function processForm() {
+
+var myMsg = document.getElementById('myMsg');
+var check = document.querySelector('#check');
+
+    //store user name in a variable
+
+
+    myMsg.innerHTML = ('Congratulations! You have completed Day 1!');
+
+
+
+
+
+     //prevent page from reloading
+    return false;
+
+  }
+});
