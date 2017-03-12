db23b4038a52dd8cdc56207ed4a9e4b53429241d
miaelii <lizmiao@ucdavis.edu>
2017-03-11 20:13:07 -0800

    Add files via upload

---
+++ topicchoices/prototype3/script.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+
+    console.log('reading');
+
+alert("Right click 'inspect' and view as iPhone6!")
+
+});
