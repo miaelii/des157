a133b2a185e8bc5e48356c8e70d09421dba54985
miaelii <lizmiao@ucdavis.edu>
2017-03-13 18:47:06 -0700

    Add files via upload

--- topicchoices/prototype3/login.js
+++ topicchoices/prototype3/login.js
@@ -1,64 +1,63 @@
 // JavaScript Document
 
 document.addEventListener("DOMContentLoaded", function() {
-console.log("DOM fully loaded and parsed");
+    console.log("DOM fully loaded and parsed");
+console.log('reading');
+    // all other js here
+    // wait until page loaded before doing anything
+    window.addEventListener('load', function() {
 
-// all other js here
-// wait until page loaded before doing anything
-window.addEventListener('load', function() {
+        // sign in flow
+        var signInButton = document.getElementById('sign-in-button');
+        signInButton.addEventListener('click', function() {
+            var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(provider);
 
-    // sign in flow
-    var signInButton = document.getElementById('sign-in-button');
-    signInButton.addEventListener('click', function() {
-        var provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopup(provider);
+            console.log("signInWithPopup")
+        });
 
-        console.log("signInWithPopup")
+        // sign out
+        var signOutButton = document.getElementById('sign-out-button');
+        signOutButton.addEventListener('click', function() {
+            firebase.auth().signOut();
+
+            console.log("signOut")
+        });
+
+        // do things when a user signs in or out
+
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                // User is signed in.
+                var displayName = user.displayName;
+                var email = user.email;
+                var emailVerified = user.emailVerified;
+                var photoURL = user.photoURL;
+                var isAnonymous = user.isAnonymous;
+                var uid = user.uid;
+                var providerData = user.providerData;
+                // ...
+                var overlay = document.getElementById('overlay');
+                overlay.style.visibility = 'hidden';
+                var base = document.getElementById('base');
+                //use the alert() to concatenate a message
+                base.innerHTML = ('<h1>Welcome to Fit Fifteen! ' + displayName + '!</h1> <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
+                console.log(user.displayName);
+            } else {
+                // User is signed out.
+                // ...
+                var base = document.getElementById('base');
+                base.style.visibility = 'hidden';
+
+                console.log('User logged out');
+            }
+        });
+
+
+        var firebaseOutput = document.getElementById('firebase-output');
+        var dbRef = firebase.database().ref().child('text');
+        dbRef.on('value', snap => firebaseOutput.innerText = snap.key + ': ' + snap.val());
+
+
     });
-
-    // sign out
-    var signOutButton = document.getElementById('sign-out-button');
-    signOutButton.addEventListener('click', function() {
-        firebase.auth().signOut();
-
-        console.log("signOut")
-    });
-
-    // do things when a user signs in or out
-
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            // User is signed in.
-            var displayName = user.displayName;
-            var email = user.email;
-            var emailVerified = user.emailVerified;
-            var photoURL = user.photoURL;
-            var isAnonymous = user.isAnonymous;
-            var uid = user.uid;
-            var providerData = user.providerData;
-            // ...
-            var overlay = document.getElementById('overlay');
-            overlay.style.visibility = 'hidden';
-
-            console.log(user.displayName);
-        } else {
-            // User is signed out.
-            // ...
-            var base = document.getElementById('base');
-            base.style.visibility = 'hidden';
-
-            console.log('User logged out');
-        }
-    });
-    console.log('reading');
-    var base = document.getElementById('base');
-    //use the alert() to concatenate a message
-    base.innerHTML = ('<h1>Welcome to Fit Fifteen! ' + displayName + '!</h1> <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
-});
-var firebaseOutput = document.getElementById('firebase-output');
-var dbRef = firebase.database().ref().child('text');
-dbRef.on('value', snap => firebaseOutput.innerText = snap.key + ': ' + snap.val());
-
-
-});
 });
