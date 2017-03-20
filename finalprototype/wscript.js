936dc94a6d9d01dc28a4342ecac24084ca45c55d
miaelii <lizmiao@ucdavis.edu>
2017-03-19 18:14:03 -0700

    Add files via upload

---
+++ finalprototype/wscript.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+
+    console.log('reading');
+
+    //capture the submit event
+    document.workout.onsubmit = processForm;
+
+    //define process function
+    function processForm() {
+
+        var myMsg = document.getElementById('myMsg');
+        var check = document.getElementById('check');
+
+        //store user name in a variable
+
+
+        myMsg.innerHTML = ('Congratulations! You have completed Day 1!');
+
+        check.setAttribute('class','active');
+
+
+
+        //prevent page from reloading
+        return false;
+
+    }
+});
